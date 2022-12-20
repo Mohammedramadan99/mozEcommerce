baf214e68c446d358c42aed8d5c9857b39df274f
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-12-20 23:04:16 +0200

    dsadasd

--- components/SpecialOffer/SpecialOffer.jsx
+++ components/SpecialOffer/SpecialOffer.jsx
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux'
 function SpecialOffer()
 {
   const { allProducts } = useSelector(state => state?.products?.productsList)
-  const specialProduct = allProducts?.find(p => p._id === '63717ad9fd6daaea663d2589')
+  // const specialProduct = allProducts?.find(p => p._id === '63717ad9fd6daaea663d2589')
+  // console.log(specialProduct)
   return (
     <div className='specialOffer'>
       <div className="container">
@@ -13,13 +14,17 @@ function SpecialOffer()
             special offer
           </div>
           <div className="specialOffer__left__name">
-            {specialProduct?.name}
+            {/* {specialProduct?.name} */}
+            Quesadilla
           </div>
           <div className="specialOffer__left__category">
-            {specialProduct?.category}
+            {/* {specialProduct?.category} */}
+            mexican food
+
           </div>
           <div className="specialOffer__left__price">
-            ${specialProduct?.price}
+            {/* ${specialProduct?.price} */}
+            $ 25
           </div>
           <div className="specialOffer__left__btn">
             add to cart
@@ -27,7 +32,7 @@ function SpecialOffer()
         </div>
         <div className="specialOffer__right">
           <div className="specialOffer__right__img">
-            {specialProduct?.images[0]?.url && <Image src={specialProduct?.images[0]?.url} layout="fill" objectFit='contain' alt="img" />}
+            <Image src='https://res.cloudinary.com/dtmjc8y9z/image/upload/v1668381401/ecommerce/g3bgyk9qmbjgdshf6mxw.png' layout="fill" objectFit='contain' alt="img" />
             
           </div>
         </div>

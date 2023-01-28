6d7798f42a5720d2c9532a7dcec5977f1a4bf686
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-28 16:03:52 +0200

    da

--- utils/auth.jsx
+++ utils/auth.jsx
@@ -1,63 +1,30 @@
 import jwt from "jsonwebtoken";
 import User from "../Modal/userModel";
 import db from '../utils/db/dbConnect'
-const signToken = (user) =>
-{
-  console.log("ss");
-  console.log(process.env.JWT_SECRET);
-  return jwt.sign(
-    {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
+export const isAuth = async (req, res, next) => {
+  try
+  {
+    await db.connect()
+    let token = req.headers.authorization.split(" ")[1];
 
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "30d",
+    if (!token) {
+      return res.status(403).send("Access Denied");
     }
-  );
-};
-const isAuth = async (req, res, next) =>
-{
-  await db.connect()
-  let token;
-  if (req?.headers?.authorization?.startsWith("Bearer"))
-  {
-    token = req.headers.authorization.split(" ")[1];
-    try
-    {
-      if (token)
-      {
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
-        const { id } = decoded
-        //find the user by id
-        const user = await User.findById(decoded?.id).select("-password");
-        console.log("is Auth: " + user._id);
-        //attach the user to the request object
-        req.user = user;
-        next();
-      }
-    } catch (error)
-    {
-      throw new Error(error.message);
-    }
-  } else
-  {
-    throw new Error("There is no token attached to the header");
-  }
-};
 
-const isAdmin = async (req, res, next) =>
-{
-  if (req.user.isAdmin)
-  {
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(verified?.id).select("-password");
+    console.log("user", user)
+    req.user = user;
     next();
-  } else
-  {
-    res.status(401).send({ message: "User is not admin" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
+  // finally
+  // {
+  //   // await db.disconnect();
+  // }
 };
-
-export { signToken, isAuth, isAdmin };

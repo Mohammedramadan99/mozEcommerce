0fc30b46fed6dbe8a4bca45d50f7294ca1db225c
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-01-28 13:29:35 +0200

    da

--- components/Layouts/MainLayout.jsx
+++ components/Layouts/MainLayout.jsx
@@ -15,13 +15,6 @@ export default function MainLayout({ title, children })
     <>
       <Head>
         <title>{title ? title + " - moz" : "moz"}</title>
-        <meta name="description" content="Ecommerce Website" />
-        <link rel="icon" href="/favicon.ico" />
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=The+Nautigal:wght@400;700&display=swap" rel="stylesheet" />
-
       </Head>
       <header>
         <Navbar/>

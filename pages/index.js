7f09eebb7d7869aa48526cbb5278a2ec1734db8e
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-11 16:04:19 +0200

    Add ssr option to disable server-rendering

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,56 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProductsAction } from "../store/productsSlice";
 import dynamic from "next/dynamic";
 import { Spinner } from "react-bootstrap";
-const Banner = dynamic(() => import("../components/Banner/Banner")); 
-const AllCategories = dynamic(() => import("../components/Categories/AllCategories"));
-const Categories = dynamic(() => import("../components/Categories/Categories"));
-const CustomerSays = dynamic(() => import("../components/CustomerSays/CustomerSays"));
-const Features = dynamic(() => import("../components/Features/Features"));
-const ProductsSlider = dynamic(() => import("../components/Products/ProductsSlider"));
-const SpecialOffer = dynamic(() => import("../components/SpecialOffer/SpecialOffer"));
+const Banner = dynamic(() => import("../components/Banner/Banner"), {
+  ssr: false,
+});
+const AllCategories = dynamic(
+  () => import("../components/Categories/AllCategories"),
+  {
+    ssr: false,
+  }
+);
+const Categories = dynamic(
+  () => import("../components/Categories/Categories"),
+  {
+    ssr: false,
+  }
+);
+const CustomerSays = dynamic(
+  () => import("../components/CustomerSays/CustomerSays"),
+  {
+    ssr: false,
+  }
+);
+const Features = dynamic(() => import("../components/Features/Features"), {
+  ssr: false,
+});
+const ProductsSlider = dynamic(
+  () => import("../components/Products/ProductsSlider"),
+  {
+    ssr: false,
+  }
+);
+const SpecialOffer = dynamic(
+  () => import("../components/SpecialOffer/SpecialOffer"),
+  {
+    ssr: false,
+  }
+);
 
-export default function Home()
-{
-  const dispatch = useDispatch()
-  const { productsList:{ allProducts } } = useSelector((state) => state.products);
+export default function Home() {
+  const dispatch = useDispatch();
+  const {
+    productsList: { allProducts },
+  } = useSelector((state) => state.products);
   const { loading: productsLoading } = useSelector((state) => state.products);
-  const { loading: categoryLoading } = useSelector((state) => state.category);
   // const { loading: reviewusLoading } = useSelector(
   //   (state) => state.globalReviews
   // );
   // const loading = productsLoading || categoryLoading ? true : false
-  useEffect(() => 
-  {
+  useEffect(() => {
     dispatch(fetchProductsAction());
-  }, [])
+  }, []);
   return (
     <div>
       {productsLoading ? (
